87af4bd595582cdd287fd027e630778da0a78543
SumSumarie <winniecws191216@gmail.com>
2024-01-23 10:43:38 +0100

    update the main page

---
+++ src/helper.py
@@ -0,0 +1,29 @@
+from PIL import Image, ImageTk
+import tkinter as tk
+
+
+def clear_widgets(root):
+    """This function will destroy any widgets you created"""
+    for i in root.winfo_children():
+        i.destroy()
+
+
+def add_image(root, file_path, width, height):
+    """This definition will place the image on the gui window.
+    You need to specify the variable name that creates your gui window and the image file path"""
+
+    # for some reason this image will not appear without specifying global variables
+    global pic, f1
+
+    # Create the frame for the first windows of the game
+    f1 = tk.Frame(root)
+    # read the image you want to use for the first fra,e
+    img = Image.open(file_path)
+    # resize the image - make sure this is the same size as the gui window
+    img = img.resize((width, height), Image.LANCZOS)
+    # add this code to view the image as the frame
+    pic = ImageTk.PhotoImage(img)
+    Lab = tk.Label(f1, image=pic)
+    # code to just place the image
+    Lab.pack()
+    f1.pack()
